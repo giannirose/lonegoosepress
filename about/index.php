ce96eac2bf78f953f7469263886fb1c6e57a6664
John Rose <john.rose3@comcast.net>
2018-09-10 19:03:10 -0700

    The About file for creating subpages
    To create subpages in Perch with the regions already entered.

---
+++ about/index.php
@@ -0,0 +1,8 @@
+<?php include ('../perch/runtime.php');?>
+	<?php perch_layout('global/header'); ?>
+		<?php perch_content('Main Navigation'); ?>
+			<div class="about article">
+				<?php perch_content('About'); ?>	
+			</div>
+		<?php perch_content('Main Navigation'); ?>
+<?php perch_layout('global/footer'); ?>
