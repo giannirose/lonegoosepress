10a2bd4027a504e194962d75d610fab18a274ada
John Rose <john.rose3@comcast.net>
2018-10-13 06:55:51 -0700

    Update products-lg.php

--- pages/shop/products-lg.php
+++ pages/shop/products-lg.php
@@ -1,36 +1,47 @@
 <?php if (!defined('PERCH_RUNWAY')) include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
+
+<?php
+   $catSlug = perch_get('category');
+
+   if($catSlug) {
+      // category view
+      $cat = perch_category("products/$catSlug/", ['skip-template' => true]);
+  
+      // if the category doesn't exist, use 404 page
+      if(!$cat) {
+         PerchSystem::use_error_page(404);
+      exit;
+      }
+
+   } else {
+    // regular view - i.e. /shop
+   }
+?>
+
 <?php perch_layout('global/header'); ?>
+   <?php perch_content('Main Navigation'); ?>
 
-	<?php perch_content('Main Navigation'); ?>
-
-		<div class="folio">
-
-			<div class="center-content">
-				<h2 class="text-center"><?php perch_content('Page Headline'); ?></h2>	
-				<?php perch_content('Page Intro'); ?>
-			</div>
-
-				<aside class="aside-left"></aside><!--Even if unused, this maintains the margins and look from the home page-->
-				<aside class="aside-right"></aside><!--Even if unused, this maintains the margins and look from the home page-->
-		</div><!--end class="folio"-->
-
-		<div class="folio-list">
-			<?php perch_shop_products([
-    			'template' => '/products/folio_featured_item_shop.html',
-			]); ?>
-		</div>
-
-    	<div class="folio-list-shop product boxer">
-
-			<?php perch_shop_products([
-    			'template' => '/products/list_lg.html',
-    			'category' => 'products/books',
-    			'sort' => 'sorting_number',
-    			'sort-order' => 'ASC',
-    			'sort-type' => 'numeric',
-    			'variants' => true,
-			]); ?>
-
-    	</div><!--Close div class=folio-list product boxer-->
- 
+<!--Product and store pages-->
+<!--   <div class="folio-list-shop product boxer">-->
+      <?php
+         if($catSlug) {
+            perch_shop_products([
+               'category' => "products/$catSlug/",
+               'template' => '/products/list_lg.html',
+               'sort' => 'sorting_number',
+               'sort-order' => 'ASC',
+               'sort-type' => 'numeric',
+            ]);
+         } else {
+            perch_shop_products([
+               'sort' => 'sorting_number',
+               'template' => '/products/list.html',
+               'sort' => 'sorting_number',
+               'sort-order' => 'ASC',
+               'sort-type' => 'numeric',
+            ]);
+         }
+      ?>
+<!--   </div>-->
+     
 <?php perch_layout('global/footer'); ?>
