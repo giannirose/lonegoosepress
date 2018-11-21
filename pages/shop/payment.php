dd64a2b03cb35f80256e84cbce8457000d46a81b
John Rose <john.rose3@comcast.net>
2018-11-21 07:16:49 -0800

    Update payment.php

--- pages/shop/payment.php
+++ pages/shop/payment.php
@@ -1,29 +1,35 @@
 <?php
-	perch_layout('global.above-lg', array(
-			'section_heading' => 'shop',
-		));
+   if(perch_shop_successful_order_id()) {
+   // successful payment
+      perch_shop_empty_cart();
+      perch_member_log_out();
+   }
 ?>
 
-<?php
-  perch_shop_complete_payment('paypal-express');
+   <?php
+	   perch_layout('global.above-lg', array(
+		    'section_heading' => 'shop',
+		  ));
+  ?>
 
-  if (perch_shop_order_successful()) {
-      echo '<h2>Thank you for your order!</h2>';
-    }else{
-      echo '<h2>Sorry!</h2>';
-    }
-
-    perch_shop_empty_cart();
-?>
-
-<?php
-    if (perch_member_is_passwordless()) {
-        perch_member_form('set_passwordless.html');
-    }
-?>
-
-
+   <?php
+      perch_shop_complete_payment('paypal-express');
+         if (perch_shop_order_successful()) {
+            echo '<h2>Thank you for your order!</h2>';
+         }else{
+            echo '<h2>Sorry!</h2>';
+      }
+   ?>
+    
+<!--
+      <?php
+         // if (perch_member_is_passwordless()) {
+         //perch_member_form('set_passwordless.html');
+         // }
+      ?>
+-->
 <?php
 	perch_layout('global.below');
 ?>
+
 
