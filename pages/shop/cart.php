2eb36f562070b3823aeeeb7294415fe0f03b5c78
John Rose <john.rose3@comcast.net>
2018-11-29 22:30:40 -0800

    Update cart.php

--- pages/shop/cart.php
+++ pages/shop/cart.php
@@ -3,20 +3,29 @@
 			'section_heading' => 'shop',
 		));
 ?>
-<!--By removing perch_layout('global/header'); in the php statement above, the layout reverts to the Nest layout with a sidebar. Other results are due to the CSS that is applied-->
+
 <?php
 	PerchSystem::set_var('empty_cart_message', perch_content('Your cart is empty', true));
 	perch_shop_cart([
 		'template' => 'cart/cart.html',
 		]);
+?>
+<!--
+<?php
 //	perch_layout('shop.sidebar', array(
 //			'promo' => false,
 //			'shipping' => false,
-//			'currency' => true,
+//			'currency' => false,
 //		));
-
 ?>
-
+-->
+<!--
 <?php
-	perch_layout('global.below');
+//	perch_layout('global.below');
 ?>
+-->
+
+				</div>
+			</div>
+		</body>
+	</html>
