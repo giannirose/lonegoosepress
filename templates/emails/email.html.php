96bf2b90ea4b932e159936d3cb660905c0662cdc
John Rose <john.rose3@comcast.net>
2025-12-23 16:32:47 -0800

    Add files via upload

---
+++ templates/emails/email.html.php
@@ -0,0 +1,8 @@
+Sandy, you received the following from your website contact form:
+
+<p><?= $text ?></p>
+
+<p>Have a nice day,</p>
+<p>Name: <?= $sender ?></p>
+<p><?= $sender ?>'s email: <?= $email ?></p>
+
---
+++ templates/emails/email.php
@@ -0,0 +1,5 @@
+<h2>Hello Company,</h2>
+
+<?= $text ?>
+
+<?= $sender ?>
