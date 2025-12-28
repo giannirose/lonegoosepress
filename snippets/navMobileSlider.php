c2b3d6a3106a377c46cbb76c50ce0293a41d16a3
John Rose <john.rose3@comcast.net>
2025-12-28 12:09:33 -0800

    Create navMobileSlider.php

---
+++ snippets/navMobileSlider.php
@@ -0,0 +1,37 @@
+<button id="menu-toggle" class="mobile-toggle">☰ Menu</button>
+<div id="menu-overlay"></div>
+
+<nav id="mobile-nav">
+  <button id="menu-close" class="mobile-close">✕ Close</button>
+
+  <ul>
+    <li><a href="/">Home</a></li>
+
+    <li class="heading">Publications</li>
+    <ul class="submenu">
+      <li><a href="/books">Books</a></li>
+      <li><a href="/broadsides">Broadsides</a></li>
+      <li><a href="/ephemera">Ephemera</a></li>
+    </ul>
+    <hr>
+
+    <li class="heading">About</li>
+    <ul class="submenu">
+      <li><a href="/about">About the Press</a></li>
+      <li><a href="/about-the-proprietor">Sandy Tilcock, Proprietor</a></li>
+    </ul>
+    <hr>
+
+    <li class="heading">Commissions</li>
+    <ul class="submenu">
+      <li><a href="/design">Design</a></li>
+      <li><a href="/realization">Realization</a></li>
+      <li><a href="/publishing-support">Publishing Support</a></li>
+    </ul>
+    <hr>
+
+    <li><a href="/impressions">Impressions</a></li>
+    <li><a href="/contact">Contact</a></li>
+    <li><a href="/cart">Cart</a></li>
+  </ul>
+</nav>
