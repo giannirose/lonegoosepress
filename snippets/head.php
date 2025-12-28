904b71b4b4a0355f7aabb87f41634ce02170edc6
John Rose <john.rose3@comcast.net>
2025-12-28 12:12:59 -0800

    Update head.php when creating Mobile Slider

--- snippets/head.php
+++ snippets/head.php
@@ -7,51 +7,17 @@
   <title><?= $site->title() ?></title>
   <meta name="description" content="Website for lonegoosepress, a letterpress company owned and run by Sandy Tilcock">
   <meta name="keywords" content="letterpress">
+  <link rel="icon" type="image/x-icon" href="../assets/images/favicon.ico">
+  <link rel="stylesheet" href="../assets/css/aareset.css" type="text/css">
+  <link rel="stylesheet" href="../assets/css/lonegoose-gridflex.css" type="text/css">
+  <link rel="stylesheet" href="../assets/css/blockquote.css" type="text/css">
 
-  <?= css('assets/css/index.css') ?>
-  <link rel="stylesheet" href="assets/css/lonegoose.css" type="text/css">
-  <!-- <link rel="stylesheet" href="assets/css/lonegoose-gridflex.css" type="text/css"> -->
-  <link rel="stylesheet" href="assets/css/shop-lg.css" type="text/css">
-  <link rel="stylesheet" href="assets/css/blog.css" type="text/css">
-  <link rel="stylesheet" href="assets/css/blockquote.css" type="text/css">
-  <?= css('assets/css/aareset.css') ?>
-
-  <link rel="stylesheet" href="assets/css/accordion_cp01.css" type="text/css">
-  <?= css('assets/css/accordion.css') ?>
-  <?= css('assets/css/style.css') ?>
-  <?= css('assets/css/modalhf.css') ?>
-
-  <link href="assets/books.json">
-
-  <style>
-    form {
-      max-width: 500px;
-      margin: auto;
-      padding: 1rem;
-      border: 1px solid #ccc;
-      border-radius: 8px;
-      font-family: Arial, sans-serif;
-    }
-
-    label {
-      display: block;
-      margin-top: 1rem;
-    }
-
-    input,
-    textarea,
-    button {
-      width: 100%;
-      padding: 0.5rem;
-      margin-top: 0.25rem;
-      box-sizing: border-box;
-    }
-
-    .hidden {
-      display: none;
-    }
-  </style>
-
-
-
+  <link rel="stylesheet" href="../assets/css/accordion_cp01.css" type="text/css">
+  <link rel="stylesheet" href="../assets/css/accordion.css" type="text/css">
+  <link rel="stylesheet" href="../assets/css/lonegoose.css" type="text/css">
+  <link rel="stylesheet" href="../assets/css/style.css" type="text/css">
+  <link rel="stylesheet" href="../assets/css/modalhf.css" type="text/css">
+  <link rel="stylesheet" href="../assets/css/navMobileCss.css" type="text/css">
+  <link rel="stylesheet" href="../assets/css/navAccordionSliderCss.css" type="text/css">
+  <link rel="stylesheet" href="../assets/css/navMobileSliderCss.css" type="text/css">
 </head>
