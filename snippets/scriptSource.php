a20d79f2c432daaa738b79c9c13a8e6294a2c142
John Rose <john.rose3@comcast.net>
2025-12-28 12:11:47 -0800

    Update scriptSource.php when creating mobile slider

--- snippets/scriptSource.php
+++ snippets/scriptSource.php
@@ -1,3 +1,4 @@
 <script src="../assets/js/accordion-nojquery1.js" type="text/javascript"></script>
 <script src="../assets/js/navMobile.js" type="text/javascript"></script>
-<script src="../assets/js/accordion-1.js" type="text/javascript"></script>
+<script src="../assets/js/navAccordionSliderJs.js" type="text/javascript"></script>
+<script src="../assets/js/navMobileSliderJs.js" type="text/javascript"></script>
