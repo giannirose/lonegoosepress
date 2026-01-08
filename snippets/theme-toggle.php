845f8d1611c944978178c74c5591a4d0a47a9e38
John Rose <john.rose3@comcast.net>
2026-01-08 08:03:27 -0800

    Create theme-toggle.php

---
+++ snippets/theme-toggle.php
@@ -0,0 +1,22 @@
+<div class="theme-toggle">
+  <input type="radio" name="theme" id="theme-light" value="light" checked>
+  <label for="theme-light">
+
+    <span class="icon">☀️</span>
+    Light
+  </label>
+
+  <input type="radio" name="theme" id="theme-auto" value="auto">
+  <label for="theme-auto">
+    Auto
+  </label>
+
+  <input type="radio" name="theme" id="theme-dark" value="dark">
+  <label for="theme-dark">
+    Dark
+    <span class="icon">🌙</span>
+
+  </label>
+
+  <div class="highlight"></div>
+</div>
