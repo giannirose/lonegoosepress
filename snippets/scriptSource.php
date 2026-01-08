d89eab72f0c7cd57548f318a9eca055d23305bdb
John Rose <john.rose3@comcast.net>
2026-01-08 08:00:35 -0800

    Update scriptSource.php add light/dark mode

--- snippets/scriptSource.php
+++ snippets/scriptSource.php
@@ -2,3 +2,4 @@
 <script src="../assets/js/navMobile.js" type="text/javascript"></script>
 <script src="../assets/js/navAccordionSliderJs.js" type="text/javascript"></script>
 <script src="../assets/js/navMobileSliderJs.js" type="text/javascript"></script>
+<script src="/assets/js/theme.js"></script>
