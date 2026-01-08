1e7afdf6cfe9b7afe892ec1d87ec20b6af1315bc
John Rose <john.rose3@comcast.net>
2026-01-08 08:02:21 -0800

    Update smallHead.php / add light/dark mode

--- snippets/smallHead.php
+++ snippets/smallHead.php
@@ -1,10 +1,15 @@
-<div class="smallHead">
+<div class="theme-container">
+  <?= snippet('theme-toggle') ?>
+</div>
+<header class="smallHead">
   <div class="logo">
-    <img class="logo" src="assets/images/lonegoose.png" alt="lone goose image" width="80" height="80" />
+
+    <img class="logo" src="../assets/images/lonegoose_trans.png" alt="the Canadian goose logo" width="80" height="80">
+
   </div>
   <div class="headTitle">
     <a href="../index.html">
       <p class="h1">lone goose press</p>
     </a>
   </div>
-</div>
+</header>
